37ff9dbc5db0f93516fcbb0811a727cdc55ee7f5
JCP@ZJU310 <jcp0578@gmail.com>
2019-03-10 21:07:21 +0800

    update longestIncreasingPath - AC but too slow

--- Leecode for JAVA/src/longestIncreasingPath/Solution.java
+++ Leecode for JAVA/src/longestIncreasingPath/Solution.java
@@ -1,12 +1,14 @@
 package longestIncreasingPath;
 
+import java.util.Arrays;
+
 /*
  * 矩阵中的最长递增路径
  * https://leetcode-cn.com/explore/interview/card/top-interview-quesitons-in-2018/272/dynamic-programming/1181/
  * 
- * 回溯法，穷举，利用二维数据记录路径
- * out time
- * 134/137
+ * 动态规划,从单节点考虑，
+ * AC but too slow
+ * 93ms 5.71%
  * 
  */
 public class Solution {
@@ -21,60 +23,78 @@ public class Solution {
 		if (len_x == 0)
 			return 0;
 
-		int out_t = 0;
-		for (int i = 0; i < len_y; i++) {
-			for (int j = 0; j < len_x; j++) {
-				// 与上一行的对比
-				if (i > 0) {
-					if (matrix[i][j] > matrix[i - 1][j])
+		long need_nums = len_x * len_y;
+		int out_t = 1;
+		int[][] matrix_out = new int[len_y][len_x];
+
+		for (int[] temp : matrix_out) {
+			Arrays.fill(temp, 1);
+		}
+		int[][] matrix_flag = new int[len_y][len_x];// 标识数据是否更新
+		while (need_nums != 0) {
+			for (int i = 0; i < len_y; i++) {
+				for (int j = 0; j < len_x; j++) {
+					if (matrix_flag[i][j] == 1)// 不需要更新
 						continue;
+
+					int flag = 0;
+					if (i > 0) {
+						if (matrix[i][j] > matrix[i - 1][j]) {
+							matrix_out[i][j] = Math.max(matrix_out[i][j], matrix_out[i - 1][j] + 1);
+							if (matrix_flag[i - 1][j] == 0)
+								flag = 1;
+						}
+					}
+					if (i < len_y - 1) {
+						if (matrix[i][j] > matrix[i + 1][j]) {
+							matrix_out[i][j] = Math.max(matrix_out[i][j], matrix_out[i + 1][j] + 1);
+							if (matrix_flag[i + 1][j] == 0)
+								flag = 1;
+						}
+					}
+					if (j > 0) {
+						if (matrix[i][j] > matrix[i][j - 1]) {
+							matrix_out[i][j] = Math.max(matrix_out[i][j], matrix_out[i][j - 1] + 1);
+							if (matrix_flag[i][j - 1] == 0)
+								flag = 1;
+						}
+					}
+					if (j < len_x - 1) {
+						if (matrix[i][j] > matrix[i][j + 1]) {
+							matrix_out[i][j] = Math.max(matrix_out[i][j], matrix_out[i][j + 1] + 1);
+							if (matrix_flag[i][j + 1] == 0)
+								flag = 1;
+						}
+					}
+					if (matrix_out[i][j] > out_t)
+						out_t = matrix_out[i][j];
+					if (flag == 0) {
+						need_nums--;
+						matrix_flag[i][j] = 1;
+					}
 				}
-				// 与左一行的对比
-				if (j > 0) {
-					if (matrix[i][j] >matrix[i][j - 1])
-						continue;
-				}
-				int[][] matrix_use = new int[len_y][len_x];
-				int temp = find(matrix, matrix_use, i, j,Integer.MIN_VALUE);
-				out_t = (out_t > temp) ? out_t : temp;
 			}
 		}
 		return out_t;
-
-	}
-
-	private static int find(int[][] matrix, int[][] matrix_use, int i, int j,int last_val) {
-		// 越界
-		if (i < 0 || i >= len_y)
-			return 0;
-		if (j < 0 || j >= len_x)
-			return 0;
-		// 已使用
-		if (matrix_use[i][j] == 1)
-			return 0;
-		
-		if(matrix[i][j] <=last_val)
-			return 0;
-		
-		int out_temp = 0;
-		matrix_use[i][j] = 1;
-		
-		out_temp = Math.max(out_temp, 1 + find(matrix, matrix_use, i + 1, j,matrix[i][j]));
-
-		out_temp = Math.max(out_temp, 1 + find(matrix, matrix_use, i - 1, j,matrix[i][j]));
-
-		out_temp = Math.max(out_temp, 1 + find(matrix, matrix_use, i, j + 1,matrix[i][j]));
-
-		out_temp = Math.max(out_temp, 1 + find(matrix, matrix_use, i, j - 1,matrix[i][j]));
-		
-		matrix_use[i][j] = 0;
-		return out_temp;
 	}
 
 	public static void main(String[] args) {
-		int[][] nums_in = { {7,8,9},{9,7,6},{7,2,3}};
-		System.out.println(longestIncreasingPath(nums_in));
+		int[][] nums_in = { { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 19, 18, 17, 16, 15, 14, 13, 12, 11, 10 },
+				{ 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 }, { 39, 38, 37, 36, 35, 34, 33, 32, 31, 30 },
+				{ 40, 41, 42, 43, 44, 45, 46, 47, 48, 49 }, { 59, 58, 57, 56, 55, 54, 53, 52, 51, 50 },
+				{ 60, 61, 62, 63, 64, 65, 66, 67, 68, 69 }, { 79, 78, 77, 76, 75, 74, 73, 72, 71, 70 },
+				{ 80, 81, 82, 83, 84, 85, 86, 87, 88, 89 }, { 99, 98, 97, 96, 95, 94, 93, 92, 91, 90 },
+				{ 100, 101, 102, 103, 104, 105, 106, 107, 108, 109 },
+				{ 119, 118, 117, 116, 115, 114, 113, 112, 111, 110 },
+				{ 120, 121, 122, 123, 124, 125, 126, 127, 128, 129 },
+				{ 139, 138, 137, 136, 135, 134, 133, 132, 131, 130 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
 
+//		int[][] nums_in= {{7,8,9},{9,7,6},{7,2,3}};
+		long startTime = System.currentTimeMillis();
+		System.out.println(longestIncreasingPath(nums_in));
+		long endTime = System.currentTimeMillis(); // 获取结束时间
+
+		System.out.println("程序运行时间：" + (endTime - startTime) + "ms"); // 输出程序运行时间
 	}
 
 }
