beee39700be4a82efe2d7df700baaed52c9b9cc0
JCP@ZJU310 <jcp0578@gmail.com>
2019-03-13 20:13:07 +0800

    deleteNode -AC

---
+++ Leecode for JAVA/src/deleteNode/Solution.java
@@ -0,0 +1,23 @@
+package deleteNode;
+
+import util.ListNode;
+
+/*
+ * 删除链表中的节点
+ * https://leetcode-cn.com/explore/interview/card/top-interview-quesitons-in-2018/265/linked-list/1151/
+ * 链表至少包含两个节点。
+ * 链表中所有节点的值都是唯一的。
+ * 给定的节点为非末尾节点并且一定是链表中的一个有效节点。
+ * 不要从你的函数中返回任何结果。
+ *
+ *
+ * AC
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+    	ListNode next_p=node.next;   //非末尾节点无需检查
+    	node.val=next_p.val;
+    	node.next=next_p.next;
+    	next_p.next=null;
+    }
+}
