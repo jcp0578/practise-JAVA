2bad998a6fdc379a63b711ee8b723683b298f4e8
JCP@ZJU310 <jcp0578@gmail.com>
2019-03-15 22:07:22 +0800

    Updata reverseBits to AC

--- Leecode for JAVA/src/reverseBits/Soluiton.java
+++ Leecode for JAVA/src/reverseBits/Soluiton.java
@@ -2,31 +2,27 @@ package reverseBits;
 /*
    *  颠倒二进制位
  *  
- * AC but too slow 
+ * AC 
  */
 public class Soluiton {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-    	if(n==1)
-    	{
-    		return Integer.MIN_VALUE;
-    	}
-        StringBuilder out_t=new StringBuilder(Integer.toBinaryString(n));
-        out_t.reverse();
-        for(int i=out_t.length();i<32;i++)
-        	out_t.append('0');
-        if(out_t.length()==32 && out_t.charAt(0)=='1')
-        {
-        	out_t.setCharAt(0, '-');
-        }
-        Integer out=Integer.valueOf(out_t.toString(),2);
-        if(out<0)
-        	out=-2147483648-out;
-        return out;
+        int res=Integer.reverse(n);
+        return res;
     }
+    
+//    public int reverseBits(int n) {
+//        int res=0;
+//        for(int i=0;i<32;i++){
+//            res<<=1;
+//            res|=(n&1);     //注意顺序，不然把最高位给左移出去了
+//            n>>>=1;
+//        }
+//        return res;
+//    }
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		int num_in1 = -1;
+		int num_in1 = 1;
 		Soluiton test = new Soluiton();
 		System.out.println(test.reverseBits(num_in1));
 	}
