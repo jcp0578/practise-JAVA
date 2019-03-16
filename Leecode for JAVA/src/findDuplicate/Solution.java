6c5ca955d852284ccc87ed272c50323c58be14f5
JCP@ZJU310 <jcp0578@gmail.com>
2019-03-16 21:53:17 +0800

    findDuplicate - AC
    good idea

--- Leecode for JAVA/src/findDuplicate/Solution.java
+++ Leecode for JAVA/src/findDuplicate/Solution.java
@@ -2,27 +2,34 @@ package findDuplicate;
 
 /*
    *  寻找重复数
- * 
- * AC but too slow
-   *  时间复杂度O(N^2)
+   *  快慢指针思想, fast 和 slow 是指针, nums[slow] 表示取指针对应的元素
+   *  注意 nums 数组中的数字都是在 1 到 n 之间的(在数组中进行游走不会越界),
+   *  因为有重复数字的出现, 所以这个游走必然是成环的, 环的入口就是重复的元素, 
+   *  即按照寻找链表环入口的思路来做
+ *  
+ * AC 
+   *  时间复杂度O(N)
  */
 public class Solution {
     public int findDuplicate(int[] nums) {
-        int len=nums.length;
-        if(len<2)
-        	return -1;
-        if(len==2)
-        	return nums[0];
-        
-        for(int i=0;i<len;i++)
-        {
-        	for(int j=i+1;j<len;j++)
-        	{
-        		if(nums[i]==nums[j])
-        			return nums[i];
-        	}
+    	int len=nums.length;
+    	if(len<2)
+    		return -1;
+    	if(len==2)
+    		return nums[0];
+        int fast = 0, slow = 0;
+        while(true) {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+            if(slow == fast) {
+                fast = 0;
+                while(nums[slow] != nums[fast]) {
+                    fast = nums[fast];
+                    slow = nums[slow];
+                }
+                return nums[slow];
+            }
         }
-        return nums[len-1];
     }
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
