a0f5667e36e5a336e79507b1527ca6db4c08cecb
JCP@ZJU310 <jcp0578@gmail.com>
2019-03-17 20:05:41 +0800

    numDupDigitsAtMostN - error

--- Leecode for JAVA/src/numDupDigitsAtMostN/Solution.java
+++ Leecode for JAVA/src/numDupDigitsAtMostN/Solution.java
@@ -3,33 +3,32 @@ package numDupDigitsAtMostN;
 
 /**
  * 
- * out time
+ * ERROR
  * @author JCP
  *
  */
 public class Solution {
+	int find_N=0;
     public int numDupDigitsAtMostN(int N) {
-        int out=0;
-        for(int i=11;i<=N;i++)
-        {
-        	int[] count=new int[10];
-        	int j=i;
-        	while(j>0)
-        	{
-        		count[j%10]++;
-        		if(count[j%10]==2)
-        		{
-        			out++;
-        			break;
-        		}
-        		j/=10;
-        	}
-        }
-        return out;
+    	find_N=N;
+    	return N+1-numBoDupDigit(0,0);
     }
+	private int numBoDupDigit(int mask, int num) {
+		if(num>find_N)
+			return 0;
+		int out=1;
+		for(int i=((num==0)?1:0);i<10;i++)
+		{
+			if(((mask>>i)&1)==0)
+			{
+				out+=numBoDupDigit(mask|(1<<i),num*10+i);
+			}
+		}
+		return out;
+	}
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		int num_in=1000;
+		int num_in=221518701;
 		Solution test=new Solution();
 		System.out.println(test.numDupDigitsAtMostN(num_in));
 	}
