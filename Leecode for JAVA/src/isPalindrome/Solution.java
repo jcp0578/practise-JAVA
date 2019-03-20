e956dcefdfd4fa43ddd0aed26531d1f0ae3d56ad
JCP@ZJU310 <jcp0578@gmail.com>
2019-03-20 14:20:52 +0800

    Updata isPalindrome  to AC

--- Leecode for JAVA/src/isPalindrome/Solution.java
+++ Leecode for JAVA/src/isPalindrome/Solution.java
@@ -2,44 +2,45 @@ package isPalindrome;
 
 /**
  * 125. 验证回文串
- * 
- * @author JCP
- * AC but slow
+   *   重构
+ * AC 
+ * 6ms 92.68%
  */
 public class Solution {
-    public boolean isPalindrome(String s) {
-        int len=s.length();
-        if(len<=1)
-        	return true;
-        int i=0,j=len-1;
-        while(i<j)
-        {
-        	char temp_i='@';
-        	for(;i<len;)
-        	{
-        		temp_i=s.charAt(i++);
-        		if(Character.isDigit(temp_i) || Character.isLetter(temp_i))
-    				break;
-        		temp_i='@';
-        	}
-        	temp_i=Character.toLowerCase(temp_i);
-        	char temp_j='@';
-        	for(;j>=0;)
-        	{
-        		temp_j=s.charAt(j--);
-        		if(Character.isDigit(temp_j) || Character.isLetter(temp_j))
-    				break;
-        		temp_j='@';
-        	}
-        	temp_j=Character.toLowerCase(temp_j);
-    		if(temp_i!=temp_j)
-    			return false;
-        }
-        return true;
-    }
+	public boolean isPalindrome(String s) {
+		int len = s.length();
+		if (len <= 1)
+			return true;
+		int i = 0, j = len - 1;
+		while (true) {
+			if (i >= j)
+				return true;
+			int start = (int) s.charAt(i);
+			if (start >= 'A' && start <= 'Z')
+				start += 32;
+			if ((start >= 'a' && start <= 'z') || (start >= '0' && start <= '9')) {
+				int end = (int) s.charAt(j);
+				if (end >= 'A' && end <= 'Z')
+					end += 32;
+				if ((end >= 'a' && end <= 'z') || (end >= '0' && end <= '9')) {
+					if (start != end)
+						return false;
+					else {
+						i++;
+						j--;
+					}
+				} else {
+					j--;
+				}
+			} else {
+				i++;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-		Solution test=new Solution();
-		String test_str= ".,";
+		Solution test = new Solution();
+		String test_str = "A man, a plan, a canal: Panama";
 		System.out.println(test.isPalindrome(test_str));
 	}
 
