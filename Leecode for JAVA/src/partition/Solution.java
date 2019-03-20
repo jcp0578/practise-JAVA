e9a9a00cfeb0044e3cc4b2a6f8f671fec2892916
JCP@ZJU310 <jcp0578@gmail.com>
2019-03-20 15:17:36 +0800

    Updata partition - AC (DSF)

--- Leecode for JAVA/src/partition/Solution.java
+++ Leecode for JAVA/src/partition/Solution.java
@@ -6,8 +6,9 @@ import java.util.ArrayList;
 /*
  * 分割回文串
  * DFS
- * AC but slow
- * 13ms 51.48%
+   *   优化ispartition，减少无用的子字符串的生成
+ * AC
+ * 7ms 88.13%
  * 
  */
 public class Solution {
@@ -15,34 +16,30 @@ public class Solution {
 
 	public List<List<String>> partition(String s) {
 		ret = new ArrayList<>();
-		int len = s.length();
 		ArrayList<String> add_t = new ArrayList<>();
-		dsp(add_t, s, 0, len);
+		DFS(add_t, s, 0);
 		return ret;
 	}
 
-	private void dsp(ArrayList<String> add_t, String s, int start, int end) {
-		if (start == end) {
-			ArrayList<String> add = new ArrayList<>(add_t);
-			ret.add(add);
-			return;
-		}
-		for (int i = start + 1; i <= end; i++) {
-			String head_t = s.substring(start, i);
-			if (ispartition(head_t)) {
-				add_t.add(head_t);
-				dsp(add_t, s, i, end);
-				add_t.remove(add_t.size()-1);
-			} else {
+	private void DFS(ArrayList<String> add_t, String s, int start) {
+		if (start >= s.length())
+			ret.add(new ArrayList<>(add_t));
+		else {
+			for (int i = start + 1; i <= s.length(); i++) {
+				if (ispartition(s, start, i)) {
+					String head_t = s.substring(start, i);
+					add_t.add(head_t);
+					DFS(add_t, s, i);
+					add_t.remove(add_t.size() - 1);
+				} else {
+				}
 			}
 		}
-
 	}
 
-	private boolean ispartition(String s) {
-		int len = s.length();
-		int i = 0;
-		int j = len - 1;
+	private boolean ispartition(String s, int start, int end) {
+		int i = start;
+		int j = end - 1;
 		while (i <= j) {
 			if (s.charAt(i) == s.charAt(j)) {
 				i++;
@@ -55,7 +52,7 @@ public class Solution {
 
 	public static void main(String[] args) {
 		Solution test = new Solution();
-		String test_str = "cbbbccc";
+		String test_str = "aaa";
 		System.out.println(test.partition(test_str));
 	}
 
