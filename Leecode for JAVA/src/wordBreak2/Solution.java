bdc00d3c50b94591978d2d152ad4c9278664b156
JCP@ZJU310 <jcp0578@gmail.com>
2019-03-21 23:30:25 +0800

    perf  wordBreak2 - AC but slow (DP & DSF)

--- Leecode for JAVA/src/wordBreak2/Solution.java
+++ Leecode for JAVA/src/wordBreak2/Solution.java
@@ -7,9 +7,10 @@ import java.util.List;
  * 
  * 单词拆分2
  * 
- * DFS
+ * DP判断是否存在，DSF输出
  * 
- * out time
+ * AC but slow
+ * 33ms - 31.67%
  * 
  */
 public class Solution {
@@ -21,11 +22,58 @@ public class Solution {
 		this._dict=wordDict;
 		this.out=new ArrayList<String>();
 		ArrayList<String> add_t=new ArrayList<>();
-		check(0,add_t);
+		
+		int len=s.length();
+		boolean[] flag=new boolean[len+1];
+		flag[0]=true;
+		
+		int word_len_max=0;
+		for(String t:_dict)
+		{
+			word_len_max=Math.max(word_len_max, t.length());
+		}
+		
+		for(int i=1;i<=len;i++)
+		{
+			for(int j=Math.max(0, i-word_len_max);j<i;j++)
+			{
+				if(flag[j])
+				{
+					if(check(j,i))
+						flag[i]=true;
+				}
+			}
+		}
+    	if(flag[len])
+    		DSF(0,add_t);
     	return out;
         
     }
-	private void check(int start, ArrayList<String> add_t) {
+	private boolean check(int start,int end) {
+		if(start>=end)
+			return false;
+		for(String t:_dict)
+		{
+			int i=0;
+			int t_len=t.length();
+			if((end-start)!=t_len)
+				continue;
+			for(i=0;i<t_len;i++)
+			{
+
+				if(t.charAt(i)!=_str.charAt(start+i))
+				{
+					break;
+				}
+			}
+			if(i==t_len)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	private void DSF(int start, ArrayList<String> add_t) {
 		if(start==_str.length())
 		{
 			if(add_t.isEmpty())
@@ -63,20 +111,25 @@ public class Solution {
 				if(i==t_len)
 				{
 					add_t.add(t);
-					check(start+t_len,add_t);
+					DSF(start+t_len,add_t);
 					add_t.remove(add_t.size()-1);
 				}
 			}			
 		}
 	}
 	public static void main(String[] args) {
-		String test_in="catsanddog";
+		String test_in="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 		List<String> wordDict_t=new ArrayList<>();
-		wordDict_t.add("cat");
-		wordDict_t.add("cats");
-		wordDict_t.add("and");
-		wordDict_t.add("sand");
-		wordDict_t.add("dog");
+		wordDict_t.add("a");
+		wordDict_t.add("aa");
+		wordDict_t.add("aaa");
+		wordDict_t.add("aaaa");
+		wordDict_t.add("aaaaa");
+		wordDict_t.add("aaaaaa");
+		wordDict_t.add("aaaaaaa");
+		wordDict_t.add("aaaaaaaa");
+		wordDict_t.add("aaaaaaaaa");
+		wordDict_t.add("aaaaaaaaaa");
 		Solution test=new Solution();
 		System.out.println(test.wordBreak(test_in, wordDict_t));
 
