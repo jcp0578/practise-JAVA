7e302b138151784f447629db3748f5dd02949e33
JCP@ZJU310 <jcp0578@gmail.com>
2019-03-26 15:52:03 +0800

    pref topKFrequent - AC

--- Leecode for JAVA/src/topKFrequent/Solution.java
+++ Leecode for JAVA/src/topKFrequent/Solution.java
@@ -1,7 +1,6 @@
 package topKFrequent;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
@@ -10,8 +9,9 @@ import java.util.HashMap;
  * 前K个高频元素
  * HashMap统计，后根据value排序
  * 
- * AC but slow
- * 95ms - 19.41%
+ * AC but have bug
+ * 当元素出现频率相同时，会多输出
+ * 28ms - 84.16%
  * 
  */
 public class Solution {
@@ -22,21 +22,26 @@ public class Solution {
 		for (int i = 0; i < nums.length; i++) {
 			count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
 		}
-		
-        // 将map.entrySet()转换成list
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(count.entrySet());
-        // 通过比较器来实现排序
-        Collections.sort(list, (a,b)->(b.getValue()-a.getValue()));	
-		for(int i=0;i<list.size() && i<k;i++)
-		{
-			ret.add(list.get(i).getKey());
+		int[] frequency = new int[nums.length + 1];
+		for (Integer t : count.values()) {
+			frequency[t]++;
+		}
+		int threshold = nums.length;
+		int index_t = nums.length;
+		while (k > 0) {
+			k -= frequency[index_t];
+			threshold = index_t--;
+		}
+		for (Integer key_t : count.keySet()) {
+			if (count.get(key_t) >= threshold)
+				ret.add(key_t);
 		}
 		return ret;
 	}
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		int[] test_in = { 1, 1, 1, 2, 2, 3 };
+		int[] test_in = { 1, 1, 1, 2, 2, 2, 3, 3, 3 };
 		int k = 2;
 		Solution test = new Solution();
 		for (int t : test.topKFrequent(test_in, k)) {
