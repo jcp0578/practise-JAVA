f0f5cd775410b528f867611d75cc630084cf77af
JCP@ZJU310 <jcp0578@gmail.com>
2019-03-26 20:03:03 +0800

    pref - evalRPN - AC

--- Leecode for JAVA/src/evalRPN/Solution.java
+++ Leecode for JAVA/src/evalRPN/Solution.java
@@ -2,40 +2,43 @@ package evalRPN;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Stack;
 
 /*
  * 逆波兰表达式求值
- * AC but too slow
- * 49ms - 7.75%
+ * Integer.parseInt(str)花费时间较多
+ * AC
+ * 10ms - 75.83%
  * 
  */
 public class Solution {
     public int evalRPN(String[] tokens) {
-    	Deque<Integer> operands = new ArrayDeque<>();
+    	Deque<Integer> operands= new ArrayDeque<>();
         for(String str:tokens)
         {
-        	str=str.trim();
-        	try {
-        		operands.push(Integer.valueOf(str));
-        	}
-        	catch(NumberFormatException e)
+        	if(str.length()==1 && str.charAt(0)<'0')
         	{
     			int temp2=operands.pop();
     			int temp1=operands.pop();
-    			switch(str)
+    			char t=str.charAt(0);
+    			switch(t)
     			{
-    				case "+":operands.push(temp1+temp2);break;
-    				case "-":operands.push(temp1-temp2);break;
-    				case "*":operands.push(temp1*temp2);break;
-    				case "/":operands.push(temp1/temp2);break;
+    				case '+':operands.push(temp1+temp2);break;
+    				case '-':operands.push(temp1-temp2);break;
+    				case '*':operands.push(temp1*temp2);break;
+    				case '/':operands.push(temp1/temp2);break;
     				default:break;
     			}        		
+        	}
+        	else
+        	{
+        		operands.push(Integer.parseInt(str));//
         	}
         }
         return operands.peek();
     }
 	public static void main(String[] args) {
-		String[] test_in = {"10", "6", " 9     ", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
+		String[] test_in = {"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
 		Solution test = new Solution();
 		System.out.println(test.evalRPN(test_in));
 
