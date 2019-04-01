4bb2db392e810e41b3dce847830c1e1388e65d3e
JCP@ZJU310 <jcp0578@gmail.com>
2019-04-01 21:21:32 +0800

    isInterleave - out time

--- Leecode for JAVA/src/isInterleave/Solution.java
+++ Leecode for JAVA/src/isInterleave/Solution.java
@@ -2,35 +2,54 @@ package isInterleave;
 
 /*
  * 交错字符串
- * error
- * 存在bug
+ * 递归
+ * out time
+ * 
  */
 public class Solution {
+	String _s1;
+	String _s2;
+	String _s3;
+
 	public boolean isInterleave(String s1, String s2, String s3) {
 		if (s3.length() != (s1.length() + s2.length()))
 			return false;
+		this._s1 = s1;
+		this._s2 = s2;
+		this._s3 = s3;
 
 		int s1_i = 0, s2_i = 0, s3_i = 0;
-		while (s1_i < s1.length() && s2_i < s2.length() && s3_i < s3.length()) {
-			if (s1.charAt(s1_i) == s3.charAt(s3_i)) {
-				s1_i++;
-				s3_i++;
-			} else if (s2.charAt(s2_i) == s3.charAt(s3_i)) {
-				s2_i++;
-				s3_i++;
+
+		return find(s1_i, s2_i, s3_i);
+
+	}
+
+	private boolean find(int s1_i, int s2_i, int s3_i) {
+		if (s3_i == this._s3.length())
+			return true;
+		if (s1_i == this._s1.length()) {
+			return this._s3.substring(s3_i).equals(this._s2.substring(s2_i));
+		}
+		if (s2_i == this._s2.length()) {
+			return this._s3.substring(s3_i).equals(this._s1.substring(s1_i));
+		}
+		if (this._s1.charAt(s1_i) == this._s2.charAt(s2_i)) {
+			if (this._s1.charAt(s1_i) == this._s3.charAt(s3_i)) {
+				return (this.find(s1_i + 1, s2_i, s3_i + 1) | this.find(s1_i, s2_i + 1, s3_i + 1));
 			} else
 				return false;
-		}
-		if (s1_i == s1.length() && s2_i == s2.length() && s3_i == s3.length())
-			return true;
-		else
+		} else if (this._s1.charAt(s1_i) == this._s3.charAt(s3_i)) {
+			return this.find(s1_i + 1, s2_i, s3_i + 1);
+		} else if (this._s2.charAt(s2_i) == this._s3.charAt(s3_i)) {
+			return this.find(s1_i, s2_i + 1, s3_i + 1);
+		} else
 			return false;
 	}
 
 	public static void main(String[] args) {
 		String s1 = "aabcc";
 		String s2 = "dbbca";
-		String s3 = "aadbbcbcac";
+		String s3 = "aadbbbaccc";
 		Solution test = new Solution();
 		System.out.println(test.isInterleave(s1, s2, s3));
 
