dfbbf0da5c87b58d82d55eacf7687fc1908f0215
JCP@ZJU310 <jcp0578@gmail.com>
2019-04-05 18:17:40 +0800

    pref - sumSubarrayMins - out -time

--- Leecode for JAVA/src/sumSubarrayMins/Solution.java
+++ Leecode for JAVA/src/sumSubarrayMins/Solution.java
@@ -2,39 +2,38 @@ package sumSubarrayMins;
 
 /*
  * 子数组的最小值之和
- * 利用最小堆
- * 时间复杂度O(N^2logN) 
+ * 
+ * 时间复杂度O(N^2) 
  * out time
  */
-import java.util.PriorityQueue;
 
 public class Solution {
-    public int sumSubarrayMins(int[] A) {
-        int sum=0;
-        for(int i=0;i<A.length;i++)
-        {
-        	sum+=deal(A,i);
-        	if(sum>=(1000000007))
-        	{
-        		sum%=(1000000007);
-        	}
-        }
-        return sum;
-    }
+	public int sumSubarrayMins(int[] A) {
+		int sum = 0;
+		for (int i = 0; i < A.length; i++) {
+			sum += deal(A, i);
+			if (sum >= (1000000007)) {
+				sum %= (1000000007);
+			}
+		}
+		return sum;
+	}
+
 	private int deal(int[] a, int start) {
-		PriorityQueue<Integer> queue=new PriorityQueue<>();
-		int sum=0;
-		for(int i=start;i<a.length;i++)
-		{
-			queue.add(a[i]);
-			sum+=queue.peek();
-		}	
+		int sum = 0;
+		int min_t = a[start];
+		for (int i = start; i < a.length; i++) {
+			if (min_t > a[i]) {
+				min_t = a[i];
+			}
+			sum += min_t;
+		}
 		return sum;
 	}
 
 	public static void main(String[] args) {
-		int[] test_in= {85};
-		Solution test=new Solution();
+		int[] test_in = {1,2 };
+		Solution test = new Solution();
 		System.out.println(test.sumSubarrayMins(test_in));
 	}
 }
