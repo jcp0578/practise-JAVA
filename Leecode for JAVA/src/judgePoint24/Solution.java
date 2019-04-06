86def39e77dd547c31a246a122ca8d732d6ebf3c
JCP@ZJU310 <jcp0578@gmail.com>
2019-04-06 23:17:55 +0800

    judgePoint24 - AC but slow

--- Leecode for JAVA/src/judgePoint24/Solution.java
+++ Leecode for JAVA/src/judgePoint24/Solution.java
@@ -1,72 +1,66 @@
 package judgePoint24;
 
+import java.util.ArrayList;
+
 /*
  * 24点游戏
- * 存在bug
- * 
+ * AC but slow
+ * 35ms 
  */
 public class Solution {
 	public boolean judgePoint24(int[] nums) {
-		boolean[] visit = new boolean[4];
-		for (int i = 0; i < 4; i++) {
-			double last = nums[i];
-			visit[i] = true;
-			if (DFS(nums, visit, last))
-				return true;
-			visit[i] = false;
+		ArrayList<Double> nums_t=new ArrayList<>();
+		for(int t:nums)
+		{
+			nums_t.add(new Double(t));
 		}
-		return false;
+		return deal(nums_t);
 
 	}
 
-	private boolean DFS(int[] nums, boolean[] visit, double last) {
-		boolean over_falg = true;
-		for (int i = 0; i < 4; i++) {
-			double temp = last;
-			if (visit[i])
-				continue;
-			over_falg = false;
-			visit[i] = true;
-			temp = last;
-			temp += nums[i];
-			if (DFS(nums, visit, temp))
+	private boolean deal(ArrayList<Double> nums) {
+		if(nums.size()==0)
+			return false;
+		if(nums.size()==1)
+		{
+			if(Math.abs(nums.get(0) - 24.0) < 1e-6)
 				return true;
-
-			temp = last;
-			temp -= nums[i];
-			if (DFS(nums, visit, temp))
-				return true;
-
-			temp = last;
-			temp = nums[i] - temp;
-			if (DFS(nums, visit, temp))
-				return true;
-
-			temp = last;
-			temp *= nums[i];
-			if (DFS(nums, visit, temp))
-				return true;
-			if (nums[i] != 0) {
-				temp = last;
-				temp /= nums[i];
-				if (DFS(nums, visit, temp))
-					return true;
-			}
-			if (last != 0) {
-				temp = last;
-				temp = nums[i] / last;
-				if (DFS(nums, visit, temp))
-					return true;
-			}
-			visit[i] = false;
+			else
+				return false;
 		}
-		if (over_falg) {
-			if (Math.abs(last - 24.0) < (1e-6))
-				return true;
+		for(int i=0;i<nums.size();i++)
+		{
+			for(int j=0;j<nums.size();j++)
+			{
+				if(j==i)
+					continue;
+				ArrayList<Double> temp=new ArrayList<>();
+				for(int k=0;k<nums.size();k++)
+				{
+					if(k==i || k==j)
+						continue;
+					temp.add(nums.get(k));
+				}
+                for (int k = 0; k < 4; k++) {
+                    if (k < 2 && j > i) continue;
+                    if (k == 0) temp.add(nums.get(i) + nums.get(j));
+                    if (k == 1) temp.add(nums.get(i) * nums.get(j));
+                    if (k == 2) temp.add(nums.get(i) - nums.get(j));
+                    if (k == 3) {
+                        if (nums.get(j) != 0) {
+                        	temp.add(nums.get(i) / nums.get(j));
+                        } else {
+                            continue;
+                        }
+                    }
+                    if (deal(temp)) return true;
+                    temp.remove(temp.size() - 1);
+                }				
+				
+			}
 		}
 		return false;
 	}
-
 	public static void main(String[] args) {
 		int[] test_in = { 1, 9,1,2 };
 		Solution test = new Solution();
