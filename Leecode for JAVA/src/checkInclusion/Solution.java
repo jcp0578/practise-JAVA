4070025ea3530f4e6ca1ad4ea5e518c144476054
JCP@ZJU310 <jcp0578@gmail.com>
2019-04-18 12:28:56 +0800

    perf - checkInclusion - AC

--- Leecode for JAVA/src/checkInclusion/Solution.java
+++ Leecode for JAVA/src/checkInclusion/Solution.java
@@ -1,12 +1,12 @@
 package checkInclusion;
 
-
 /*
  * 字符串的排列
  * 滑窗
  * 利用count[26]统计,比较s2中与s1相同长度的子串是否相同
  * AC 
  * 11ms - 95.38%
+ * 优化空间
  */
 public class Solution {
 	public boolean checkInclusion(String s1, String s2) {
@@ -21,37 +21,36 @@ public class Solution {
 		if (len_s1 > len_s2)
 			return false;
 
-		int[] count_s1 = new int[26];
-		int[] count_s2 = new int[26];
+		int[] count = new int[26];
 
 		for (int i = 0; i < len_s1; i++) {
-			count_s1[s1.charAt(i) - 'a']++;
+			count[s1.charAt(i) - 'a']++;
 		}
 		for (int i = 0; i < len_s1; i++) {
-			count_s2[s2.charAt(i) - 'a']++;
+			count[s2.charAt(i) - 'a']--;
 		}
-		if (judge(count_s1, count_s2))
+		if (judge(count))
 			return true;
 		for (int i = len_s1; i < s2.length(); i++) {
-			count_s2[s2.charAt(i) - 'a']++;
-			count_s2[s2.charAt(i - len_s1) - 'a']--;
-			if (judge(count_s1, count_s2))
+			count[s2.charAt(i) - 'a']--;
+			count[s2.charAt(i - len_s1) - 'a']++;
+			if (judge(count))
 				return true;
 		}
 		return false;
 	}
 
-	private boolean judge(int[] count_s1, int[] count_s2) {
+	private boolean judge(int[] count) {
 		for (int i = 0; i < 26; i++) {
-			if (count_s1[i] != count_s2[i])
+			if (count[i] != 0)
 				return false;
 		}
 		return true;
 	}
 
 	public static void main(String[] args) {
-		String test_in1 = "adsasd";
-		String test_in2 = "asdasdsadas";
+		String test_in1 = "aa";
+		String test_in2 = "baab";
 		Solution test = new Solution();
 		System.out.println(test.checkInclusion(test_in1, test_in2));
 
