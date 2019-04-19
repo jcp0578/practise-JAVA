96ceb556b1c45d18e311214c5b47f074cdf015c0
JCP@ZJU310 <jcp0578@gmail.com>
2019-04-19 12:59:01 +0800

    perf - maximalSquare - AC (DP)

--- Leecode for JAVA/src/maximalSquare/Solution.java
+++ Leecode for JAVA/src/maximalSquare/Solution.java
@@ -2,8 +2,9 @@ package maximalSquare;
 
 /*
  * 最大正方形
- * 利用row_ones与line_ones记录每点的行列方向上的连续1的个数
- * min_t=min(row_ones[i]line_ones[i]), 取(i-min_t)~i范围上line_ones的最小值，以判断是否构成正方形
+ * DP
+ * dp[j][i] = Math.min(Math.min(dp[j - 1][i], dp[j][i - 1]), dp[i - 1][j - 1]) + 1;
+ * 
  * AC
  * 10ms - 89.56%
  * 
@@ -20,32 +21,17 @@ public class Solution {
 			return 0;
 
 		int res = 0;
-		int[] row_ones = new int[len_l];
-		int[] line_ones = new int[len_l];
+		int[][] dp = new int[len_r][len_l];
 		for (int j = 0; j < len_r; j++) {
 			for (int i = 0; i < len_l; i++) {
-				// 记录每点的行列方向上的连续1的个数
-
-				if (matrix[j][i] == '1')
-					line_ones[i] = line_ones[i] + 1;
-				else
-					line_ones[i] = 0;
-
-				if (matrix[j][i] == '1')
-					row_ones[i] = (i == 0) ? 1 : (row_ones[i - 1] + 1);
-				else
-					row_ones[i] = 0;
-
-				int min_t = Math.min(line_ones[i], row_ones[i]);
-				if (min_t > res) {
-					// 判断可以构成正方形的边长
-					int temp = min_t;
-					for (int k = i; k > (i - temp); k--) {
-						if (line_ones[k] < temp)
-							temp = line_ones[k];
+				if (matrix[j][i] == '1') {
+					if (j == 0 || i == 0)
+						dp[j][i] = 1;
+					else {
+						dp[j][i] = Math.min(Math.min(dp[j - 1][i], dp[j][i - 1]), dp[j - 1][i - 1]) + 1;
 					}
-					if (temp > res)
-						res = temp;
+					if (dp[j][i] > res)
+						res = dp[j][i];
 				}
 			}
 		}
@@ -53,8 +39,7 @@ public class Solution {
 	}
 
 	public static void main(String[] args) {
-		char[][] test_in = { { '0', '1', '1', '0', '1' }, { '1', '1', '0', '1', '0' }, { '0', '1', '1', '1', '0' },
-				{ '1', '1', '1', '1', '0' }, { '1', '1', '1', '1', '1' }, { '0', '0', '0', '0', '0' } };
+		char[][] test_in = { { '0', '1', '1', '0', '1' }, { '1', '1', '0', '1', '0' } };
 		Solution test = new Solution();
 		System.out.println(test.maximalSquare(test_in));
 
