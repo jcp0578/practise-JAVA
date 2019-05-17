060b6f2f2b5f6e165b0c1015240457728d97fc31
JCP@ZJU310 <jcp0578@gmail.com>
2019-05-17 11:17:11 +0800

    isSymmetric - AC

--- Leecode for JAVA/src/isSymmetric/Solution.java
+++ Leecode for JAVA/src/isSymmetric/Solution.java
@@ -7,31 +7,23 @@ import util.Wrapper;
 
 /*
  * 对称二叉树
- * error
+ * AC
+ * 1ms - 99.61%
  */
 public class Solution {
 	public boolean isSymmetric(TreeNode root) {
-		ArrayList<Integer> pre = new ArrayList<>();
-		preorder(root, pre);
-		int len = pre.size();
-		for (int i = 0; i < len / 2; i++) {
-			if (pre.get(i) == pre.get(len - i - 1))
-				continue;
-			else
-				return false;
-		}
-		return true;
-
+		if (root == null)
+			return true;
+		return isSymmetric(root.left, root.right);
 	}
 
-	private void preorder(TreeNode node, ArrayList<Integer> pre) {
-		if (node == null)
-			return;
-		preorder(node.left, pre);
-		pre.add(node.val);
-		preorder(node.right, pre);
-		return;
-	}
+	public boolean isSymmetric(TreeNode p, TreeNode q) {
+	    if(p == null || q == null)
+	        return p == q;
+	    if(p.val != q.val)
+	        return false;
+            return (isSymmetric(p.left, q.right) && isSymmetric(p.right, q.left));//左子树先访问左节点，右子树先访问右节点
+    }
 
 	public static void main(String[] args) {
 		String test_in_str = "[1,2,2,2,null,2]";
